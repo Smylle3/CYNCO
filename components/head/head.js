7f1c839e6ee516f9ae4d388c8632e53918a35dbd
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-23 15:43:18 -0300

    Componete header

---
+++ components/head/head.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function MyHead() {
+  return (
+    <Head>
+      <title>CYNCO</title>
+      <meta name="description" content="The word game" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
